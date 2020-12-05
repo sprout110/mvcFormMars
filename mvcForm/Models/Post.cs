4da16f7af7256a60777f2fe0aa233e3af90198f5
sprout110@gmail.com <sprout110@gmail.com>
2020-12-05 12:40:39 +0800

    Add project files.

---
+++ mvcForm/Models/Post.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mvcForm.Models
+{
+    public class Post
+    {
+        public string Title { get; set; }
+        //[Display(Name = "Release Date")]
+        //[DataType(DataType.Date)]
+        //[DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0: yyyy-MM-dd}")]
+        //[DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd/MM/yyyy HH:mm}")]
+        //[DisplayFormat(DataFormatString = "{0:yyyy-MM-ddTHH:mm}", ApplyFormatInEditMode = true)]
+        public DateTime ReleaseDate { get; set; }
+        public string Description { get; set; }
+    }
+}
